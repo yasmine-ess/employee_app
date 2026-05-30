9d1dc52ee99207500d1f48dfc3b7b6d32cceb135
yasmine-ess <yasmine.esseghir28@gmailcom>
2026-05-30 18:55:35 +0100

    feat: modal suppression + toast succès + validation formulaire

--- src/app/components/employee-list/employee-list.ts
+++ src/app/components/employee-list/employee-list.ts
@@ -13,6 +13,9 @@ import { EmployeeService } from '../../services/employee.service';
 })
 export class EmployeeListComponent implements OnInit {
   employees: Employee[] = [];
+  showToast = false;
+  showModal = false;
+  employeeToDelete: Employee | undefined;
 
   constructor(private employeeService: EmployeeService , private router: Router) {}
  // ngOnInit est une méthode qui s'exécute automatiquement quand le composant se charge , comme page_load en asp.net
@@ -20,6 +23,10 @@ export class EmployeeListComponent implements OnInit {
   ngOnInit(): void {
     //afficher tout les employés dès le chargement du composant
     this.employees = this.employeeService.getAll(); 
+     // Affiche le toast si on revient du formulaire
+    if (history.state?.success) {
+      this.showSuccessToast();
+    }
   }
   goToAdd(): void {
     this.router.navigate(['/employees/add']);
@@ -29,8 +36,30 @@ export class EmployeeListComponent implements OnInit {
     this.router.navigate(['/employees/edit', id]);
   }
  // la méthode delete est appelée quand on clique sur le bouton supprimer d'un employé , elle prend l'id de l'employé à supprimer en paramètre
-  delete(id: number): void { 
-    this.employeeService.delete(id);
-    this.employees = this.employeeService.getAll();
+  delete(employee: Employee): void {
+    this.employeeToDelete = employee;
+    this.showModal = true;
   }
+  // Annule la suppression
+  cancelDelete(): void {
+    this.showModal = false;
+    this.employeeToDelete = undefined;
+  }
+
+  // Confirme la suppression
+  confirmDelete(): void {
+    if (this.employeeToDelete) {
+      this.employeeService.delete(this.employeeToDelete.id);
+      this.employees = this.employeeService.getAll();
+      this.showModal = false;
+      this.employeeToDelete = undefined;
+      this.showSuccessToast();
+    }
+  }
+  //showToast = false;
+
+private showSuccessToast(): void {
+  this.showToast = true;
+  setTimeout(() => this.showToast = false, 3000);
+}
 }
