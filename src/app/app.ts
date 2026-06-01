08c5712af3befe05843897740fbfe0177650edea
yasmine-ess <yasmine.esseghir28@gmailcom>
2026-06-01 21:18:18 +0100

    feat: ajout navbar

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { NavbarComponent } from './components/navbar/navbar';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('employee_app');
 }
+
